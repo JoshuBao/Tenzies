7cc18f1421c35df3d632f97c840952a7f13146f7
Joshua Cheng <joshuacheng@Joshuas-MacBook-Pro.local>
2022-07-05 18:34:58 -0700

    added two dependencies: nanoid and react confetti, finished game, rollDice, holdDice,generateNewDie,state for Dice and Tenzies, useEffect to put the two state in sync, players can play the game, win, then replay

--- src/App.js
+++ src/App.js
@@ -1,39 +1,82 @@
 import React from "react"
-import style from "./style.css"
 import Die from "./components/Die"
+import { nanoid } from "nanoid"
 
-/**
- * Challenge:
- * 
- * Create state to hold our array of numbers. (Initialize
- * the state by calling our `allNewDice` function so it 
- * loads all new dice as soon as the app loads)
- * 
- * Map over the state numbers array to generate our array
- * of Die elements and render those in place of our
- * manually-written 10 Die elements.
- */
+import Confetti from 'react-confetti'
+
 export default function App() {
+
+ 
   const [dice,setDice] = React.useState(allNewDice())
+  const [tenzies,setTenzies] = React.useState(false)
+  /**
+ * Challenge: Check the dice array for these winning conditions:
+ * 1. All dice are held, and
+ * 2. all dice have the same value
+ * 
+ * If both conditions are true, set `tenzies` to true and log
+ * "You won!" to the console
+ */
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzies(true)
+      console.log("You won!")
+    }
+  },[dice])
+  
+  function generateNewDie(){
+    return(
+      {value:Math.ceil(Math.random() * 6),
+        isHeld:false,
+        id: nanoid()}
+    )
+  }
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push(generateNewDie())
     }
     return newDice
   }
-  function rollDice()
-  {
-    setDice(prevDice => allNewDice())
+
+  function rollDice(){
+    setDice(prevDice => prevDice.map(die => {
+        return(
+          die.isHeld ?  
+          die :
+          generateNewDie()
+        )
+    }))
+    if(tenzies){
+      setDice(allNewDice())
+      setTenzies(false)
+    }
   }
-  const diceElements = dice.map(die => <Die value={die} />)
-  console.log(diceElements)
+  function holdDice(id){
+    return(
+      setDice(prevDice => prevDice.map(die => {
+        return die.id === id ? 
+        {...die, isHeld: !die.isHeld} : 
+        die
+      }))
+    )
+  }
+  const diceElements = dice.map(die => 
+  <Die key={die.id} holdDice={() => holdDice(die.id)} value={die.value} isHeld={die.isHeld} />)
+  
   return (
     <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice"onClick={rollDice}>Roll</button>
+      <button className="roll-dice"onClick={rollDice}>{tenzies? "New Game" : "Roll"}</button>
+      
     </main>
   )
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,13 @@
 import React from "react"
-// import style from "./style.css"
+
 export default function Die(props)
 {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391": "white"
+      }
+  
     return(
-        <div className="die-face">
+        <div onClick={props.holdDice} style={styles} className="die-face">
         <h2 className="die-num">{props.value}</h2>
         </div>
     )
